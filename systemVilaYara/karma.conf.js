d925cd78ba79e4f7015f3487bc28859438e879a5
Natália Sabino <na.ssilva@outlook.com>
2021-11-17 12:59:49 -0300

    atualização de pastas

--- systemVilaYara/karma.conf.js
+++ systemVilaYara/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/sistemaVilaYara'),
+      dir: require('path').join(__dirname, './coverage/systemVilaYara'),
       subdir: '.',
       reporters: [
         { type: 'html' },
